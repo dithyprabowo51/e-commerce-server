3175fe4efb3d29aede783dadaa91df907c8dc6ab
Adityo Prabowo <dithyprabowo51@gmail.com>
2021-02-24 18:20:07 +0700

    add endpoint login khusus admin

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const AuthController = require('../controllers/AuthController');
 
 router.post('/login', AuthController.login);
+router.post('/loginAdmin', AuthController.loginAdmin);
 router.post('/register', AuthController.register);
 
 module.exports = router;
